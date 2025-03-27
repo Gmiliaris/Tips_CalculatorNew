bccfb68949e89f9ad3140edbb314745c55aba6f6
gustavomiliaris <miliarisgustavo@gmail.com>
2025-03-27 22:04:00 +0000

    added a new activity to the project and with it a new screen that provides the result to the client and also added a logo and another image for a future background

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.tipscalculator
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tipscalculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,39 +23,48 @@ class MainActivity : AppCompatActivity() {
         }
 
         var percentage: Int = 0
-        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                percentage = 10
-            }
-        }
-        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                percentage = 15
-            }
-        }
-        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                percentage = 20
-            }
-        }
-        binding.btnCalculate.setOnClickListener {
+
+        binding.btnDone.setOnClickListener {
             val totalTableTemp = binding.tieTotal.text
-            val nPeopleTemp = binding.tieNumPeople.text
-            val percentageTemp = binding.rg_.text
+            val nPeopleTemp = binding.tieNumberOfPeople.text
+            val percentageTemp = binding.tiePercentage.text
+            if (totalTableTemp?.isEmpty() == true ||
+                nPeopleTemp?.isEmpty() == true ||
+                percentageTemp?.isEmpty() == true
+            ) {
+                Snackbar
+                    .make(binding.tieTotal, "Fill in all fields", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                val totalTable: Float = totalTableTemp.toString().toFloat()
+                val nPeople: Int = nPeopleTemp.toString().toInt()
+                val percentage: Int = percentageTemp.toString().toInt()
 
+                val totalTemp = totalTable / nPeople
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
 
-
-
-
-
-            val totalTable: Float = binding.tieTotal.text.toString().toFloat()
-            val nPeolple: Float = binding.tieNumPeople.text.toString().toFloat()
-
-            val totalTemp = totalTable / nPeolple
-            val tips = totalTemp * percentage / 100
-            val totalWithTips = totalTemp + tips
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.apply {
+                    putExtra("totalTable", totalTable)
+                    putExtra("nPeopleTemp", nPeople)
+                    putExtra("percentage", percentage)
+                    putExtra("totalAmount", totalWithTips)
+                }
+                clean()
+                startActivity(intent)
+            }
         }
+
+        binding.btnClean.setOnClickListener {
+            clean()
+        }
+    }
+
+    private fun clean() {
+        binding.tieTotal.setText("")
+        binding.tieNumberOfPeople.setText("")
+        binding.tiePercentage.setText("")
 
     }
-}
 }
